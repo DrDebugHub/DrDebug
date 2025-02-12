d0d4030a12c41f5f41b8660dd64d52ae5c2e96eb
JackyFTW <jackmbarter@gmail.com>
2025-02-12 10:26:40 -0500

    Add default prompt and startup activation event

--- src/ai/OpenAICaller.ts
+++ src/ai/OpenAICaller.ts
@@ -1,9 +1,9 @@
 import { AIRequest } from "./AIRequest";
 import { AIFeedback } from "./AIFeedback";
 import { APICaller } from "./APICaller";
-import OpenAI from "OpenAI";
+import OpenAI from "openai";
 
-class OpenAICaller implements APICaller {
+export class OpenAICaller implements APICaller {
 
     private openai: OpenAI;
 
@@ -15,12 +15,47 @@ class OpenAICaller implements APICaller {
         return !!this.openai.organization && !!this.openai.project && !!this.openai.apiKey!;
     }
 
-    sendRequest(request: AIRequest): Promise<AIFeedback> {
-        let response: AIFeedback = {request: request, text: ""}
-
+    async sendRequest(request: AIRequest): Promise<AIFeedback> {
         // TODO: implement this
+        return this.openai.chat.completions.create({
+            model: "gpt-4o-mini",
+            messages: [
+                {
+                    role: "system",
+                    content: 
+                        `
+                        You are a helpful code debugging assistant that is knowledgable on runtime and compile-time errors.
+                        
+                        The user will ask for assistance by supplying a JSON object with contextual information. The format of this request is:
+                        {
+                            prompt: string; // This is the message of assistance sent by the user.
+                        }
 
-        return new Promise(() => response);
+                        You must analyze their issue and all contextual information to eliminate the issue altogether. You MUST respond with a JSON object in the following format, even if you are confused:
+                        {
+                            text: string; // Your solution and reasoning goes here.
+                        }
+
+                        Again, you CANNOT deviate from this request/response communication protocol defined above.
+                        `
+                },
+                {
+                    role: "user",
+                    content: 
+                        `
+                        {
+                            "prompt": "${request.prompt}"
+                        }
+                        `
+                }
+            ]
+        }).then(response => {
+            let feedback: AIFeedback = {
+                request: request,
+                text: response.choices[0].message.content!
+            };
+            return feedback;
+        });
     }
 
     followUp(response: AIFeedback): Promise<AIFeedback> {
--- src/extension.ts
+++ src/extension.ts
@@ -1,23 +1,26 @@
 import * as vscode from "vscode";
-import { FakeCaller } from "./ai/FakeCaller";
+import OpenAI from "openai";
+import { OpenAICaller } from "./ai/OpenAICaller";
 
 export function activate(context: vscode.ExtensionContext) {
-	// const organizationId: string = vscode.workspace.getConfiguration("debuggingAiAssistant").get("organizationId")!;
-	// const projectId: string = vscode.workspace.getConfiguration("debuggingAiAssistant").get("projectId")!;
-	// const apiKey: string = vscode.workspace.getConfiguration("debuggingAiAssistant").get("apiKey")!;
+	const organizationId: string = vscode.workspace.getConfiguration("debuggingAiAssistant").get("organizationId")!;
+	const projectId: string = vscode.workspace.getConfiguration("debuggingAiAssistant").get("projectId")!;
+	const apiKey: string = vscode.workspace.getConfiguration("debuggingAiAssistant").get("apiKey")!;
 
-	// const openai = new OpenAI({
-	// 	organization: organizationId,
-	// 	project: projectId,
-	// 	apiKey: apiKey
-	// });
+	const openai = new OpenAI({
+		organization: organizationId,
+		project: projectId,
+		apiKey: apiKey
+	});
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
 	const disposable = vscode.commands.registerCommand('debuggingAiAssistant.helloWorld', () => {
-		vscode.window.showInformationMessage('Hello World from Debugger!'!);
-		new FakeCaller(context);
+		let caller = new OpenAICaller(openai);
+		caller.sendRequest({ prompt: "Why is print(123) not working in my file, test.js?" }).then(response => {
+			vscode.window.showInformationMessage(`Response: ${response.text}`);
+		});
 	});
 
 	context.subscriptions.push(disposable);
