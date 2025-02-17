0da603b4ef11754eb676d8ae1d703db64f7a49f0
JackyFTW <jackmbarter@gmail.com>
2025-02-16 22:37:46 -0500

    Added interface, focus file, and expand feedback when show() is called

--- src/extension.ts
+++ src/extension.ts
@@ -1,28 +1,26 @@
-import * as vscode from 'vscode';
-import { OpenAICaller } from './ai/OpenAICaller';
-import { initSettings } from './settings';
-import { Inline } from './extension/Inline';
+import * as vscode from "vscode";
+import { OpenAICaller } from "./ai/OpenAICaller";
+import { initSettings } from "./settings";
+import { InlineDiagnostic } from "./extension/InlineDiagnostic";
 
 export function activate(context: vscode.ExtensionContext) {
 	initSettings();
 
-	const callAI = vscode.commands.registerCommand('debuggingAiAssistant.callAI', () => {
-		let caller = new OpenAICaller();
+	const callAI = vscode.commands.registerCommand("debuggingAiAssistant.callAI", () => {
+		let caller: OpenAICaller = new OpenAICaller();
 		caller.sendRequest({ prompt: "Why is print(123) not working in my file, test.js?" }).then(response => {
 			vscode.window.showInformationMessage(`Response: ${response.text}`);
 		});
 	});
 
-	const inlineMessage = vscode.commands.registerTextEditorCommand('debuggingAiAssistant.sendError', () => {
-		const editor = vscode.window.activeTextEditor;
-		if (editor) {
-			const inline = new Inline(editor.document.uri);
-			inline.setDiagnostic("Test message", new vscode.Range(new vscode.Position(0, 0), new vscode.Position(0, 0)));
-		}
+	const sendError = vscode.commands.registerCommand("debuggingAiAssistant.sendError", () => {
+		const file: vscode.Uri = vscode.Uri.joinPath(vscode.workspace.workspaceFolders![0].uri, "test.js");
+		const inline: InlineDiagnostic = new InlineDiagnostic(file, new vscode.Range(new vscode.Position(0, 0), new vscode.Position(0, 0)), "Test Message");
+		inline.show();
 	});
 
 	context.subscriptions.push(callAI);
-	context.subscriptions.push(inlineMessage);
+	context.subscriptions.push(sendError);
 }
 
 export function deactivate() {}
